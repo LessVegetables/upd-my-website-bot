0c74abf64d27dc9313b048668aa3d19d37cbd16d
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2026-04-24 09:37:04 +0700

    added logging

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 from os import getenv
+import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
@@ -11,6 +12,9 @@ load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
 MY_ID = getenv("MY_USER_ID")
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='updweb.log', level=logging.INFO)
+
 dp = Dispatcher()
 
 
@@ -18,8 +22,10 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def command_start_handler(message: Message) -> None:
     user_id = message.from_user.id
+    user = message.from_user
 
     if (user_id != int(MY_ID)):
+        logger.info(f'User {user.username} sent \\start ')
         return
     
     resp_message = f"Hello, Daniel! I'm your bot created with aiogram.\n{user_id}"
@@ -28,8 +34,11 @@ async def command_start_handler(message: Message) -> None:
 
 # Run the bot
 async def main() -> None:
+    logger.info('Started')
     bot = Bot(token=TOKEN)
     await dp.start_polling(bot)
+    logger.info('Finished')
+
 
 
 if __name__ == "__main__":
